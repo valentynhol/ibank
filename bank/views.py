d251e30f59fe1e29021e13aa36156bbe57872620
valentynhol <valentingolovko2006@gmail.com>
2023-06-18 21:34:08 +0300

    Fixed code generation

--- bank/views.py
+++ bank/views.py
@@ -98,10 +98,11 @@ def __generate_iban(ba_type):
 
 
 def __generate_code(length, uppercase=False):
-    symbol_list = '1 2 3 4 5 6 7 8 9 0 A B C D E F G H I G K L M N O P Q R S T U V W X Y Z'.split(' ')
-
-    if not uppercase:
-        symbol_list.append('a b c d e f g h i j k l m n o p q r s t u v w x y z')
+    if uppercase:
+        symbol_list = '1 2 3 4 5 6 7 8 9 0 A B C D E F G H I G K L M N O P Q R S T U V W X Y Z'.split(' ')
+    else:
+        symbol_list = '1 2 3 4 5 6 7 8 9 0 A B C D E F G H I G K L M N O P Q R S T U V W X Y Z ' \
+                      'a b c d e f g h i j k l m n o p q r s t u v w x y z'.split()
 
     return ''.join([random.choice(symbol_list) for _ in range(length)])
 
